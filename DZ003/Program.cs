3fb9409866cf8671b0d2ea6c468825fbb698e266
ceb <vivu1991@mail.ru>
2022-09-12 20:38:28 +0300

    Решение задачи №52

---
+++ DZ003/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintArray(int [,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] FillArray(int line, int column)
+{
+    int [,] array = new int [line,column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        array [i,j] = new Random().Next(1,10);
+    }
+    return array;
+}
+int [,] mas = FillArray(4, 4);
+
+PrintArray(mas);
+
+int [,] ArithmeticMean(int [,] massiv)
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            sum += massiv[j,i];
+        }
+        double arifmetic = sum/massiv.GetLength(1);
+        Console.WriteLine($"Среднее арифметическое {i+1} столбца равно: {Math.Round(arifmetic,1)} ");
+    }
+    return massiv;    
+}
+ArithmeticMean(mas);
+
